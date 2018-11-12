60094bf8f258ce715aa7cc2107da9a70bd6bd059
Siarhei Maksimau1 <siarhei_maksimau1@epam.com>
2018-11-12 18:58:32 +0300

    remove unnescessary modules

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@ import NewsRetriever from 'NewsRetriever.js';
 const newsRet = new NewsRetriever();
 newsRet.getAllSouces();
 
-function applyFilters() {
+export function applyFilters() {
     let sourcesIds = Array.prototype.slice.call(document.getElementsByClassName("source-checkbox"), 0) //convert to array
         .filter(c => c.checked)
         .map(c => c.id);
