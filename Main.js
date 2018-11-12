58c8050a85b22ffb0fa77ad2061cc9e27299e639
Siarhei Maksimau1 <siarhei_maksimau1@epam.com>
2018-11-12 18:26:00 +0300

    3

--- Main.js
+++ Main.js
@@ -1,8 +1,12 @@
 
 import NewsRetriever from 'NewsRetriever.js';
 
+const newsRet = new NewsRetriever();
+newsRet.getAllSouces();
 
-function() {
-    const newsRet = new NewsRetriever();
-    newsRet.getAllSouces();
-}();
+function applyFilters() {
+    let sourcesIds = Array.prototype.slice.call(document.getElementsByClassName("source-checkbox"), 0) //convert to array
+        .filter(c => c.checked)
+        .map(c => c.id);
+    newsRet.getNews(sourcesIds);
+}
