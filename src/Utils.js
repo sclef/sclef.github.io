3a07e1c2328b16ebc6899c26eea18fe06990f2c3
Siarhei Maksimau1 <siarhei_maksimau1@epam.com>
2018-11-21 14:22:38 +0300

    added async await

--- src/Utils.js
+++ src/Utils.js
@@ -1,10 +1,16 @@
 import 'whatwg-fetch';
 import 'es6-promise-promise';
+import "@babel/polyfill";
 
 class Utils {
     static sendRequestForJson(urls, callbackFunction) {
-        let promises = urls.map(url => fetch(url).then(y => y.json()));
+        let promises = urls.map(url => this.asyncFetch(url));
         Promise.all(promises).then(callbackFunction);
+    }
+
+    static async asyncFetch(url) {
+        let response = await fetch(url);
+        return response.json();
     }
 }
 export default Utils;
