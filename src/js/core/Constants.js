35492d367d3cf06f2d32949cfcceb2aa28365ebd
Siarhei Maksimau1 <siarhei_maksimau1@epam.com>
2018-11-27 12:06:31 +0300

    Constants original code backup

--- src/js/core/Constants.js
+++ src/js/core/Constants.js
@@ -23,3 +23,12 @@ _defineProperty(Constants, "NumberOfNews", 10);
 
 var _default = Constants;
 exports.default = _default;
+
+
+// class Constants {
+//     static ApiKey = "e03d718829c24339b5ea62712a181aae";
+//     static ArticlesUrl = "https://newsapi.org/v1/articles";
+//     static SourcesUrl = "https://newsapi.org/v1/sources";
+//     static NumberOfNews = 10;
+// }
+// export default Constants;
