4adc11f442dba3e6ad384c0312d8e979ce8c0eab
Siarhei Maksimau1 <siarhei_maksimau1@epam.com>
2018-11-28 13:55:18 +0300

    dev fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,8 +3,5 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './public'
-    }
+    devtool: 'inline-source-map'
 });
